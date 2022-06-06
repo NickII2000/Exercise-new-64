717e27433cf483f14780f53e18918944d8fc3781
Nick <Nick_II2000@mail.ru>
2022-06-06 21:49:06 +0600

    4-th commit

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function hideTabContent() {
         tabContent.forEach(item => {
-            item.style.display = 'none';
-            // item.classList.add('hide');
-            // item.classList.remove('show', 'fade');
+            // item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
@@ -18,16 +18,16 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showTabContent(i = 0) {
-        tabContent[i].style.display = 'block';
-        // tabContent[i].classList.add('show', 'fade');
-        // tabContent[i].classList.remove('hide');
+        // tabContent[i].style.display = 'block';
+        tabContent[i].classList.add('show', 'fade');
+        tabContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
     hideTabContent();
     showTabContent();
 
-    tabParent.addEventListener('mouseover', (event) => {
+    tabParent.addEventListener('click', (event) => {
         const target = event.target;
         if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
