a37f5a1ba6230d23fe285010bc11b49f8596a83b
Nick <Nick_II2000@mail.ru>
2022-06-13 20:43:53 +0600

    10-th commit

--- js/script.js
+++ js/script.js
@@ -42,16 +42,18 @@ window.addEventListener('DOMContentLoaded', () => {
     // Timer
 
     //const deadLine = new Date(new Date().getTime() + 10 * (24 * 60 * 60 * 1000));
-    const deadLine = '2022-07-01';
+    const deadLine = new Date() + 5000;
+    // const deadLine = '2022-07-01';
+
 
     function getTimeRemaining(endTime) {
         const t = Date.parse(endTime) - Date.parse(new Date()),
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
             // hours = Math.floor((t - days * 1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
-            // minuts = Math.floor((t - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60) / (1000 * 60)),
+            // minutes = Math.floor((t - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60) / (1000 * 60)),
             // seconds = Math.floor((t - days * 1000 * 60 * 60 * 24 - hours * 1000 * 60 * 60 - minuts * 1000 * 60) / 1000);
             hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-            minuts = Math.floor((t / (1000 * 60)) % 60),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
             seconds = Math.floor((t / 1000) % 60);
         return {
             'total': t,
@@ -60,27 +62,28 @@ window.addEventListener('DOMContentLoaded', () => {
             'minutes': minutes,
             'seconds': seconds,
         };
+    }
 
-        function setClock(selector, endTime) {
-            const timer = document.querySelector(selector),
-                days = timer.querySelector('#days'),
-                hours = timer.querySelector('#hours'),
-                minutes = timer.querySelector('#minutes'),
-                seconds = timer.querySelector('#seconds');
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
-            function updateClock() {
-                const t = getTimeRemaining(endTime);
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
 
-                days.innerHTML = t.days;
-                hours.innerHTML = t.hours;
-                minutes.innerHTML = t.minutes;
-                seconds.innerHTML = t.seconds;
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
 
-                if (t.total <= 0) {
-                    clearInterval(timeInterval);
-                }
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
             }
         }
-        setClock('.timer', deadLine);
-    });
+    }
+    setClock('.timer', deadLine);
+});
